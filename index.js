04068ab4989956a498be437d90e218a282caa5fe
zpfqazpl <43317761+zpfqazpl@users.noreply.github.com>
2018-10-11 02:15:08 +0800

    Update index.js
    ky

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ const bodyParser = require("body-parser")
 //使用body-parser
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json())
+
+app.all('*', function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); 
+res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"); 
+res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS"); 
+res.header("X-Powered-By",' 3.2.1') 
+if(req.method=="OPTIONS") res.send(200);
+/*让options请求快速返回*/ else next(); });
+
+
 //引入users.js
 
 const users= require("./routes/api/users")
