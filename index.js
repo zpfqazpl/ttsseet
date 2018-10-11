d61a49fdb06474a308109b04b0a3a735e2e81663
zpfqazpl <43317761+zpfqazpl@users.noreply.github.com>
2018-10-11 17:13:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,14 +16,7 @@ const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 //router解析
-app.use(history(
-    {
-        rewrites: [
-            { from: /^\/wap\/.*$/, to: '/index.html' }
-            ]
-    }
-))
-app.use(express.static(path.join(__dirname,'../dist')))
+
 
 
 
